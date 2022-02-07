e78de7100608b91df7919dc4ec84d28e0c5eb986
s-h-ubham <98087338+s-h-ubham@users.noreply.github.com>
2022-02-07 20:38:10 +0530

    Add files via upload

---
+++ Peer_Node_Mac.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+brew install git
+
+git clone https://github.com/PEER-Inc/peer-node-installer.git
+
+
+cd peer-node-installer
+
+git checkout Mac_Binary
+
+sudo chmod +x peer
+
+
+echo "Enter Your Node Name: "
+
+
+read x
+  
+
+sudo ./peer   --base-path /tmp/"${x}" --chain ./customSpecRaw.json   --port 30333  --ws-external   --rpc-external --rpc-cors all  --no-telemetry  --validator   --rpc-methods Unsafe   --name "${x}" --bootnodes /ip4/3.109.243.150/tcp/30333/p2p/12D3KooWQn9w8AdBSBkUkAkSjK4TDcZT8LB2pfT8FUr1CAqsfBtE#!/bin/bash
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+brew install git
+
+git clone https://github.com/PEER-Inc/peer-node-installer.git
+
+
+cd peer-node-installer
+
+git checkout Mac_Binary
+
+sudo chmod +x peer
+
+
+echo "Enter Your Node Name: "
+
+
+read x
+  
+
+sudo ./peer   --base-path /tmp/"${x}" --chain ./customSpecRaw.json   --port 30333  --ws-external   --rpc-external --rpc-cors all  --no-telemetry  --validator   --rpc-methods Unsafe   --name "${x}" --bootnodes /ip4/3.109.243.150/tcp/30333/p2p/12D3KooWQn9w8AdBSBkUkAkSjK4TDcZT8LB2pfT8FUr1CAqsfBtE
